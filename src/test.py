19e7f3503515fdcbd84af286eefd5894b93486ee
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2026-06-09 18:48:57 -0700

    add tests

--- src/test.py
+++ src/test.py
@@ -1,8 +1,30 @@
 import unittest
+from BinarySearchTree import BinarySearchTree, Node
 
 class TestCases(unittest.TestCase):
     def test(self):
-        self.assertEqual(1, 1)
+        bst = BinarySearchTree()
+        bst.insert(5)
+        bst.insert(3)
+        bst.insert(7)
+        self.assertTrue(bst.search(5))
+        self.assertTrue(bst.search(3))
+        self.assertTrue(bst.search(7))
+        self.assertFalse(bst.search(10))
+    def test2(self):
+        bst = BinarySearchTree()
+        bst.insert(5)
+        bst.insert(3)
+        bst.insert(7)
+        bst.delete(3)
+        self.assertFalse(bst.search(3))
+        self.assertTrue(bst.search(5))
+    def test3(self):
+        bst = BinarySearchTree()
+        bst.insert(5)
+        bst.insert(3)
+        bst.insert(7)
+        self.assertEqual(bst.inorder(), [3, 5, 7])
 
 if __name__ == '__main__':
     unittest.main()
